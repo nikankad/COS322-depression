0d8a9d67af987754f63a7fb3b73278b374c41be0
Nikan <nikankad@protonmail.com>
2025-10-12 18:03:50 -0400

    Update output function to include model name in saved file format

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,11 +1,11 @@
-#summarize model metric in string format and saves sample_submission_{r^2 score}.csv. id will just be the currentid+1
+#summarize model metric in string format and saves sample_submission_{modelname_roc_mse_r^2}.csv. id will just be the currentid+1
 def output(model_name, metrics: dict):
     """Print model performance metrics in a clean format."""
     print(f"\n📊 Results for {model_name}:")
     for metric, value in metrics.items():
         print(f" - {metric.capitalize()}: {value:.3f}")
 
-    #generate sample_submission_{r^2 score}.csv
+    #generate sample_submission_{modelname_roc_mse_r^2}.csv
 
 
 def pred_actual_graph():
